55520932b4c1e2b526f1b7eb22d6fe2912892f89
Azuzota <72482313+Azuzota@users.noreply.github.com>
2022-12-05 10:41:28 -0500

    Display Reviews
    Reviews now can be displayed for the store page. No CSS has been done yet.

--- caseYelp/my_app/pages/store.js
+++ caseYelp/my_app/pages/store.js
@@ -10,6 +10,7 @@ import {
     VStack,
     HStack,
     Button,
+    Spacer,
     Badge,
     useColorModeValue,
     Input,
@@ -107,49 +108,11 @@ const storeDetail = ({ store, userInf, _alltags, reviews }) => {
                                 <Text>
                                     [Menu] (Example)
                                 </Text>
-                                <Box>
-                                    <Text>
-                                        The Classic Burger ($6)
-                                    </Text>
-                                    <Text fontSize='10px'>
-                                        Beef patty, lettuce, tomatoes, pickles, cheese, onions.
-                                    </Text>
-                                </Box>
-
-                                <Box>
-                                    <Text>
-                                        The Double Burger ($8)
-                                    </Text>
-                                    <Text fontSize='10px'>
-                                        2 beef patties, lettuce, tomatoes, pickles, cheese, onions.
-                                    </Text>
-                                </Box>
-
-                                <Box>
-                                    <Text>
-                                        The Crispy Burger ($8)
-                                    </Text>
-                                    <Text fontSize='10px'>
-                                        Beef patty, bacon, lettuce, tomatoes, pickles, onions.
-                                    </Text>
-                                </Box>
-
-                                <Box>
-                                    <Text>
-                                        Combo ($14)
-                                    </Text>
-                                    <Text fontSize='10px'>
-                                        Any burger, fries, and a drink.
-                                    </Text>
-                                </Box>
                             </VStack>
                         </Box>
 
                         <Box>
                             <VStack spacing='24px' align='left'>
-                                <Text>
-                                    [Reviews] (Example)
-                                </Text>
                                 <Box rounded={'lg'} bg='white' boxShadow={'lg'} p={8}>
                                     <Formik
                                         initialValues={{ Review: "", Rate: "" }}
@@ -206,62 +169,18 @@ const storeDetail = ({ store, userInf, _alltags, reviews }) => {
                                         )}
                                     </Formik>
                                 </Box>
-                                <HStack spacing='24px'>
+
+                                <VStack spacing='24px'>
                                     {reviews.map((review) => (
-                                    <Text fontSize='16px'>
-                                        {review.review}
-                                    </Text>
+                                    <VStack align="left" justify="center" spacing='12px'>
+                                        <HStack spacing='16px'>
+                                            <Text>{review.authorName}</Text>
+                                            <Text>{review.score}</Text>
+                                        </HStack>
+                                        <Text >{review.review}</Text>
+                                    </VStack>
                                     ))}
-                                    <Box>
-                                        <Image src={UserIcon} />
-                                    </Box>
-                                    <Text fontSize='16px'>
-                                        John Smith ★★★★★
-                                    </Text>
-                                </HStack>
-                                <Text fontSize='12px'>
-                                    The food is high quality and the service is fast. You can dine in
-                                    or if you're in a rush for class, you can order ahead and you can
-                                    pick up reliably.
-
-                                </Text>
-
-                                <HStack spacing='16px'>
-                                    <Box>
-                                        <Image src={UserIcon} />
-                                    </Box>
-                                    <Text fontSize='16px'>
-                                        Jane Doe ★✰✰✰✰
-                                    </Text>
-                                </HStack>
-                                <Text fontSize='12px'>
-                                    I don't like hamburgers.
-                                </Text>
-
-                            </VStack>
-                        </Box>
-
-                        <Box>
-                            <VStack spacing='24px' align='left'>
-                                <Text>
-                                    [Q&A] (Example)
-                                </Text>
-                                <Box>
-                                    <Text fontSize='14px'>
-                                        Q: What kind of food do they sell?
-                                    </Text>
-                                    <Text fontSize='14px'>
-                                        A: They have burgers, fries, salads, and milkshakes.
-                                    </Text>
-                                </Box>
-                                <Box>
-                                    <Text fontSize='14px'>
-                                        Q: Should people with allergies avoid this restaurant?
-                                    </Text>
-                                    <Text fontSize='14px'>
-                                        A: If you have a soy allergy, you might want to avoid this restaurant because they fry their french fries in soy oil.
-                                    </Text>
-                                </Box>
+                                </VStack>
                             </VStack>
                         </Box>
                     </VStack>
